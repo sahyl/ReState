2c9b3449272bbc1de1c3a7c3577134467d118abc
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-12-28 16:13:58 +0530

    ReState

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -44,7 +44,7 @@ const Auth = () => {
 
         <View className="px-10">
           <Text className="text-base text-center uppercase font-rubik text-black-200">
-            Welcome To Real Scout
+            Welcome To ReState
           </Text>
 
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
@@ -53,7 +53,7 @@ const Auth = () => {
           </Text>
 
           <Text className="text-lg font-rubik text-black-200 text-center mt-12">
-            Login to Real Scout with Google
+            Login to ReState with Google
           </Text>
 
           <TouchableOpacity
